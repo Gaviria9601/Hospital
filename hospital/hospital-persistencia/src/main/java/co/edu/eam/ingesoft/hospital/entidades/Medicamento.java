8d4b3b67bd41af219f433acfca6a1a01b05d3b0e
Lenovo <Lenovo@192.168.1.20>
2017-05-20 11:22:07 -0500

    Medicamentos

--- hospital/hospital-persistencia/src/main/java/co/edu/eam/ingesoft/hospital/entidades/Medicamento.java
+++ hospital/hospital-persistencia/src/main/java/co/edu/eam/ingesoft/hospital/entidades/Medicamento.java
@@ -5,35 +5,52 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import co.edu.eam.ingesoft.hospital.enumeraciones.LaboratorioEnum;
+
+
+
+
+
 @Entity
-@Table(name="MEDICAMENTOS")
+@Table(name="MEDICAMENTO")
+@NamedQueries({
+@NamedQuery (name=Medicamento.LISTAR_MEDI, query="select m from Medicamento m")
+})
 public class Medicamento implements Serializable{
+	
+	public static final String LISTAR_MEDI = "ListarMedi";
 	
 	@Id
 	@Column(name="Codigo")
-	private Integer codigo;
+	private int codigo;
 	
 	@Column(name = "Nombre", length =40, nullable=false)
 	private String nombre;
 	
 	@Temporal(TemporalType.DATE)
-	@Column(name = "Fecha_expedicion", nullable=false)
-	private Date fechaExpedición;
+	@Column(name = "fecha_expedicion", nullable=false)
+	private Date fechaExpedicion;
 	
 	@Temporal(TemporalType.DATE)
-	@Column(name = "Fecha_expiracion", nullable=false)
-	private Date fechaExpiración;
+	@Column(name = "fecha_expiracion", nullable=false)
+	private Date fechaExpiracion;
 	
+	
+	@Enumerated(EnumType.STRING)
 	@Column(name = "Laboratorio", length =40, nullable=false)
-	private String laboratorio;
+	private LaboratorioEnum laboratorio;
 	
 	@Column(name = "Cantidad", length = 40, nullable=false)
 	private int cantidad;
@@ -44,30 +61,31 @@ public class Medicamento implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "Farmacia_codigo", nullable=false)
 	private Farmacia farmaciaCodigo;
-	
-	public Medicamento(){
+
+	public void setLaboratorio(LaboratorioEnum laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public LaboratorioEnum getLaboratorio() {
+		return laboratorio;
+	}
+
+
+	public Medicamento() {
 		super();
 	}
 
-	public Medicamento(Integer codigo, String nombre, Date fechaExpedición, Date fechaExpiración, String laboratorio,
-			int cantidad, boolean estado, Farmacia farmaciaCodigo) {
+	public Medicamento(String nombre, Date fechaExpedicion, Date fechaExpiracion,
+			LaboratorioEnum laboratorio, int cantidad, boolean estado, Farmacia farmaciaCodigo) {
 		super();
-		this.codigo = codigo;
+		
 		this.nombre = nombre;
-		this.fechaExpedición = fechaExpedición;
-		this.fechaExpiración = fechaExpiración;
+		this.fechaExpedicion = fechaExpedicion;
+		this.fechaExpiracion = fechaExpiracion;
 		this.laboratorio = laboratorio;
 		this.cantidad = cantidad;
 		this.estado = estado;
 		this.farmaciaCodigo = farmaciaCodigo;
-	}
-
-	public Integer getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
 	}
 
 	public String getNombre() {
@@ -78,29 +96,23 @@ public class Medicamento implements Serializable{
 		this.nombre = nombre;
 	}
 
-	public Date getFechaExpedición() {
-		return fechaExpedición;
+	public Date getFechaExpedicion() {
+		return fechaExpedicion;
 	}
 
-	public void setFechaExpedición(Date fechaExpedición) {
-		this.fechaExpedición = fechaExpedición;
+	public void setFechaExpedicion(Date fechaExpedicion) {
+		this.fechaExpedicion = fechaExpedicion;
 	}
 
-	public Date getFechaExpiración() {
-		return fechaExpiración;
+	public Date getFechaExpiracion() {
+		return fechaExpiracion;
 	}
 
-	public void setFechaExpiración(Date fechaExpiración) {
-		this.fechaExpiración = fechaExpiración;
+	public void setFechaExpiracion(Date fechaExpiracion) {
+		this.fechaExpiracion = fechaExpiracion;
 	}
 
-	public String getLaboratorio() {
-		return laboratorio;
-	}
-
-	public void setLaboratorio(String laboratorio) {
-		this.laboratorio = laboratorio;
-	}
+	
 
 	public int getCantidad() {
 		return cantidad;
@@ -126,11 +138,26 @@ public class Medicamento implements Serializable{
 		this.farmaciaCodigo = farmaciaCodigo;
 	}
 
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + cantidad;
+		result = prime * result + codigo;
+		result = prime * result + (estado ? 1231 : 1237);
+		result = prime * result + ((farmaciaCodigo == null) ? 0 : farmaciaCodigo.hashCode());
+		result = prime * result + ((fechaExpedicion == null) ? 0 : fechaExpedicion.hashCode());
+		result = prime * result + ((fechaExpiracion == null) ? 0 : fechaExpiracion.hashCode());
+		result = prime * result + ((laboratorio == null) ? 0 : laboratorio.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
 		return result;
 	}
 
@@ -143,12 +170,40 @@ public class Medicamento implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Medicamento other = (Medicamento) obj;
-		if (codigo == null) {
-			if (other.codigo != null)
+		if (cantidad != other.cantidad)
+			return false;
+		if (codigo != other.codigo)
+			return false;
+		if (estado != other.estado)
+			return false;
+		if (farmaciaCodigo == null) {
+			if (other.farmaciaCodigo != null)
 				return false;
-		} else if (!codigo.equals(other.codigo))
+		} else if (!farmaciaCodigo.equals(other.farmaciaCodigo))
+			return false;
+		if (fechaExpedicion == null) {
+			if (other.fechaExpedicion != null)
+				return false;
+		} else if (!fechaExpedicion.equals(other.fechaExpedicion))
+			return false;
+		if (fechaExpiracion == null) {
+			if (other.fechaExpiracion != null)
+				return false;
+		} else if (!fechaExpiracion.equals(other.fechaExpiracion))
+			return false;
+		if (laboratorio != other.laboratorio)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
 			return false;
 		return true;
 	}
+
+	
+	
+
+	
 
 }
